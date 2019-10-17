55657e2d274c33f85b7353d571ad86ee251ec61f
Marc <kemar53@hotmail.fr>
2019-10-17 16:37:50 +0200

    update user.js

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,25 @@ router.get('/', function(req, res, next) {
   User.find().exec((err, users) => res.json(users));
 });
 
-router.post('/', (req, res) => {
+/* POST user */
+router.post('/', function(req, res) => {
   const user = new User(req.body);
   user.save((err, user) => res.json(user));
 });
 
-router.get('/:id', (req, res) => {
+/* GET user */
+router.get('/:id', function(req, res) => {
   User.findById(req.params.id).exec((err, user) => res.json(user));
 });
 
-router.put('/:id', (req, res) => {
-  User.updateOne({ _id: ObjectId(req.params.id) }, req.body, (err, user) => res.json(user))
+/* PUT user */
+router.put('/:id', function(req, res) => {
+  User.updateOne({ _id: ObjectId(req.params.id) }, req.body, (err, user) => res.json(user));
 });
 
-router.delete('/:id', (req, res) => {
-  User.findByIdAndDelete(req.params.id, (err, station) => res.json(user))
+/* DELETE user */
+router.delete('/:id', function(req, res) => {
+  User.findByIdAndDelete(req.params.id, (err, station) => res.json(user));
 });
 
 module.exports = router;
