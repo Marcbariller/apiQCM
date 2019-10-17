bb9f89a3174286ac1c4e64b108d06118362dd3f4
Marc <kemar53@hotmail.fr>
2019-10-17 15:09:59 +0200

    users routes

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,25 @@ const User = require('../models/Users');
 
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  User.find().exec((err, users) => res.json(users));
+});
+
+router.post('/', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, user) => res.json(user));
+});
+
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id).exec((err, user) => res.json(user));
+});
+
+router.put('/:id', (req, res) => {
+  User.updateOne({ _id: ObjectId(req.params.id) }, req.body, (err, user) => res.json(user))
+});
+
+router.delete('/:id', (req, res) => {
+  User.findByIdAndDelete(req.params.id, (err, station) => res.json(user))
 });
 
 module.exports = router;
