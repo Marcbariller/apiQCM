625e5a5c632f01eda9e4bbab71526deef9dbd16c
leneveuT <leneveu.thibaut@gmail.com>
2019-11-03 20:56:34 +0100

    Correction de la relation

--- models/Users.js
+++ models/Users.js
@@ -4,9 +4,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: String,
     email: String,
-    questions: [{
-        id: {type: Schema.Types.ObjectId, ref: 'Question'}
-    }]
+    questions: [{type: Schema.Types.ObjectId, ref: 'Question'}]
 });
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', userSchema);
